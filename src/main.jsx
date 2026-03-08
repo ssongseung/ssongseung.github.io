87e6ba6caf59629c5ba2dae344e2292b135c34a3
송승현 <holic7760@naver.com>
2026-03-09 00:45:03 +0900

    404 페이지 추가

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>
 );
